54672cd60b9734281557e5b2058a5a7975b0053e
Aviscad <anthonyviscad@gmail.com>
2024-06-24 10:00:58 -0600

    Agregando Cors

--- EstadoCuenta.Api/Program.cs
+++ EstadoCuenta.Api/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddControllers().AddJsonOptions(
             opt.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
         }
     );
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -34,6 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAnyOrigin");
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
